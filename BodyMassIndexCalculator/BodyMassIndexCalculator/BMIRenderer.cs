c468498897b1a9b64716946fd293c4b8e1171734
Emanuele Firmani <emanuele.firmani@gmail.com>
2018-07-13 13:38:05 +0200

    BMI can be rendered as a descriptive string

---
+++ BodyMassIndexCalculator/BodyMassIndexCalculator/BMIRenderer.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace BodyMassIndexCalculator
+{
+    public class BMIRenderer
+    {
+        public static string Render(decimal bmi)
+        {
+            switch (bmi)
+            {
+                case decimal a when bmi < (decimal) 18.5: return $"underweight: {bmi}";
+                case decimal a when bmi >= (decimal) 25: return $"overweight: {bmi}";
+                default: return $"healthy: {bmi}";
+            }
+        }
+    }
+}
---
+++ BodyMassIndexCalculator/BodyMassIndexCalculatorTest/BMIRendererTest.cs
@@ -0,0 +1,41 @@
+﻿using BodyMassIndexCalculator;
+using FluentAssertions;
+using Xunit;
+
+namespace BodyMassIndexCalculatorTest
+{
+    public class BMIRendererTest
+    {
+        [Xunit.Theory]
+        [InlineData(1)]
+        [InlineData(10)]
+        [InlineData(18.499999)]
+        [InlineData(-7)]
+        [InlineData(0)]
+        public void should_return_underweight(decimal bmi)
+        {
+            var actual = BMIRenderer.Render(bmi);
+            actual.Should().StartWith("underweight: ");
+        }
+
+        [Xunit.Theory]
+        [InlineData(25)]
+        [InlineData(30)]
+        [InlineData(1000)]
+        public void should_return_overweight(decimal bmi)
+        {
+            var actual = BMIRenderer.Render(bmi);
+            actual.Should().StartWith("overweight: ");
+        }
+
+        [Xunit.Theory]
+        [InlineData(18.5)]
+        [InlineData(20)]
+        [InlineData(24.9999)]
+        public void should_return_healthy(decimal bmi)
+        {
+            var actual = BMIRenderer.Render(bmi);
+            actual.Should().StartWith("healthy: ");
+        }
+    }
+}
