585e92c4a25dcac64135d3814c23502b9e29ce06
Emanuele Firmani <emanuele.firmani@gmail.com>
2018-07-13 14:08:59 +0200

    Project can be started into a console

---
+++ BodyMassIndexCalculator/BodyMassIndexCalculator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace BodyMassIndexCalculator
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            var reader = new DecimalReader();
+            var weight = reader.Read("Please, write your weight (kg):");
+            var height = reader.Read("Please, write your height (m):");
+            
+            Console.WriteLine($"You are {BMIRenderer.Render(Calculator.Calculate(weight, height))}");
+        }
+    }
+}
