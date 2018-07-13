532b3baafe0f3ee65542f9c9308d8ed931da6358
Emanuele Firmani <emanuele.firmani@gmail.com>
2018-07-13 13:53:59 +0200

    DecimalReader can validate decimals

---
+++ BodyMassIndexCalculator/BodyMassIndexCalculator/DecimalReader.cs
@@ -0,0 +1,13 @@
+﻿namespace BodyMassIndexCalculator
+{
+    public class DecimalReader
+    {
+        public static (bool IsValid, decimal Value) Validate(string input)
+        {
+            if (decimal.TryParse(input, out var value))
+                return (true, value);
+
+            return (false, 0);
+        }
+    }
+}
---
+++ BodyMassIndexCalculator/BodyMassIndexCalculatorTest/DecimalReaderTest.cs
@@ -0,0 +1,32 @@
+﻿using BodyMassIndexCalculator;
+using FluentAssertions;
+using Xunit;
+
+namespace BodyMassIndexCalculatorTest
+{
+    public class DecimalReaderTest
+    {
+        [Theory]
+        [InlineData(".5", .5)]
+        [InlineData("5", 5)]
+        [InlineData("1.5", 1.5)]
+        [InlineData("1,5", 15)]
+        [InlineData("1.555", 1.555)]
+        [InlineData("1,55.5", 155.5)]
+        public void should_validate_decimal(string input, decimal expected)
+        {
+            var (valid, actual) = DecimalReader.Validate(input);
+            valid.Should().BeTrue();
+            actual.Should().Be(expected);
+        }
+
+        [Theory]
+        [InlineData(",5", 5)]
+        [InlineData("text", 1.555)]
+        public void should_notvalidate_nondecimals(string input, decimal expected)
+        {
+            var (valid, _) = DecimalReader.Validate(input);
+            valid.Should().BeFalse();
+        }
+    }
+}
