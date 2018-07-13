5edd799a27a83cf15a377e6a11f4006a40ce7f11
Emanuele Firmani <emanuele.firmani@gmail.com>
2018-07-13 14:01:14 +0200

    DecimalReader reads from console

--- BodyMassIndexCalculator/BodyMassIndexCalculator/DecimalReader.cs
+++ BodyMassIndexCalculator/BodyMassIndexCalculator/DecimalReader.cs
@@ -1,4 +1,6 @@
-﻿namespace BodyMassIndexCalculator
+﻿using System;
+
+namespace BodyMassIndexCalculator
 {
     public class DecimalReader
     {
@@ -8,6 +10,20 @@
                 return (true, value);
 
             return (false, 0);
+        }
+
+        public decimal Read()
+        {
+            var isValid = false;
+            decimal value = 0;
+            
+            while (!isValid)
+            {
+                var input = Console.ReadLine();
+                (isValid, value) = Validate(input);
+            }
+
+            return value;
         }
     }
 }
