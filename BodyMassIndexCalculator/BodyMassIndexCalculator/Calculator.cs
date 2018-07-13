6c038cf5b2f6e8e1db3b461aa7b9b5ac93125152
Emanuele Firmani <emanuele.firmani@gmail.com>
2018-07-13 09:05:18 +0200

    BodyMassIndexCalculator can calculate BMI

---
+++ BodyMassIndexCalculator/BodyMassIndexCalculator/Calculator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BodyMassIndexCalculator
+{
+    public class Calculator
+    {
+        public static decimal Calculate(int weight, decimal height)
+        {
+            var pWeight = (double) weight;
+            var pHeight = (double) height;
+
+            return Math.Round((decimal) (pWeight / pHeight / pHeight), 1);
+        }
+    }
+}
---
+++ BodyMassIndexCalculator/BodyMassIndexCalculatorTest/CalculatorTest.cs
@@ -0,0 +1,21 @@
+﻿using BodyMassIndexCalculator;
+using FluentAssertions;
+using Xunit;
+
+namespace BodyMassIndexCalculatorTest
+{
+    public class CalculatorTest
+    {
+        [Xunit.Theory]
+        [InlineData(1, 1, 1)]
+        [InlineData(25, 5, 1)]
+        [InlineData(100, 5, 4)]
+        [InlineData(70, 1.7, 24.2)]
+        public void should_calculate_bmi(int weight, decimal height, decimal expected)
+        {
+            var sut = new Calculator();
+            var actual = Calculator.Calculate(weight, height);
+            actual.Should().Be(expected);
+        }
+    }
+}
