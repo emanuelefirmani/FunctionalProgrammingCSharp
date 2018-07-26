66244c1b9dc132a9160307a4b84bc69291cba7cd
Emanuele Firmani <emanuele.firmani@gmail.com>
2018-07-26 16:14:38 +0200

    LookupFunction applies predicate to numbers for None cases

--- HonestFunctions/HonestFunctions/LookupFunction.cs
+++ HonestFunctions/HonestFunctions/LookupFunction.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using FluentAssertions;
 using LaYumba.Functional;
+using LaYumba.Functional.Option;
 using Xunit;
 
 namespace HonestFunctions
@@ -11,7 +12,7 @@ namespace HonestFunctions
     {
         public static Option<int> Lookup(IEnumerable<int> list, Func<int, bool> predicate)
         {
-            return list.First(predicate);
+            return list.Any(predicate) ? list.First(predicate) : new Option<int>();
         }
     }
 
@@ -29,6 +30,26 @@ namespace HonestFunctions
 
             input = new List<int> {0,2,1};
             LookupFunction.Lookup(input, IsOdd).Equals(1).Should().BeTrue();
+
+            input = new List<int> {0,2,3,1};
+            LookupFunction.Lookup(input, IsOdd).Equals(3).Should().BeTrue();
+        }       
+
+        [Fact]
+        public void should_return_none_for_numbers()
+        {
+            bool IsOdd(int i) => i % 2 == 1;
+            
+            List<int> input;
+            
+            input = new List<int>();
+            LookupFunction.Lookup(input, IsOdd).Equals(new None()).Should().BeTrue();
+            
+            input = new List<int> {0};
+            LookupFunction.Lookup(input, IsOdd).Equals(new None()).Should().BeTrue();
+
+            input = new List<int> {0,2,4};
+            LookupFunction.Lookup(input, IsOdd).Equals(new None()).Should().BeTrue();
         }       
     }
 }
