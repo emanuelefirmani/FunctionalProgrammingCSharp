15929722401ef1c8691471af81a16bf8b7334bb0
Emanuele Firmani <emanuele.firmani@gmail.com>
2018-07-26 15:54:53 +0200

    DayOfWeek tests are in the same file as the class

--- HonestFunctions/HonestFunctions/DayOfWeek.cs
+++ HonestFunctions/HonestFunctions/DayOfWeek.cs
@@ -1,5 +1,8 @@
 using System;
+using FluentAssertions;
 using LaYumba.Functional;
+using LaYumba.Functional.Option;
+using Xunit;
 using Enum = LaYumba.Functional.Enum;
 
 namespace HonestFunctions
@@ -9,6 +12,31 @@ namespace HonestFunctions
         public static Option<DayOfWeek> GetDay(string input)
         {
             return Enum.Parse<DayOfWeek>(input);
+        }
+    }
+    
+    public class DayOfWeekTest
+    {
+        [Theory]
+        [InlineData("Monday", DayOfWeek.Monday)]
+        [InlineData("Tuesday", DayOfWeek.Tuesday)]
+        [InlineData("Wednesday", DayOfWeek.Wednesday)]
+        [InlineData("Thursday", DayOfWeek.Thursday)]
+        [InlineData("Friday", DayOfWeek.Friday)]
+        [InlineData("Saturday", DayOfWeek.Saturday)]
+        [InlineData("Sunday", DayOfWeek.Sunday)]
+        public void should_resolve(string input, DayOfWeek expected)
+        {
+            DayOfWeekHelper.GetDay(input).Equals(expected).Should().BeTrue();
+        }
+
+        [Theory]
+        [InlineData("text")]
+        [InlineData("Montag")]
+        [InlineData("Freeday")]
+        public void should_not_resolve(string input)
+        {
+            DayOfWeekHelper.GetDay(input).Equals(new None()).Should().BeTrue();
         }
     }
 }    
