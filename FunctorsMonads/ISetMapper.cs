0223da8008b175b8e169fc032df973df19562a88
Emanuele Firmani <emanuele.firmani@gmail.com>
2018-08-08 15:03:40 +0200

    ISetMapper can map ISets

--- FunctorsMonads/ISetMapper.cs
+++ FunctorsMonads/ISetMapper.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Xunit;
 using FluentAssertions;
 
@@ -7,9 +8,9 @@ namespace FunctorsMonads
 {
     public static class ISetMapper
     {
-        public static HashSet<int> Map(this HashSet<string> input, Func<string, int> f)
+        public static HashSet<int> Map(this ISet<string> input, Func<string, int> f)
         {
-            return new HashSet<int>();
+            return input.Select(f).ToHashSet();
         }
     }
 
@@ -26,6 +27,42 @@ namespace FunctorsMonads
 
             actual.Should().NotBeNull();
             actual.Count.Should().Be(0);
+        }
+
+        [Fact]
+        public void should_map_hashset_with_one_element()
+        {
+            var sut = new HashSet<string>();
+            sut.Add("test");
+
+            int f(string s) => string.IsNullOrEmpty(s) ? 0 : s.Length;
+
+            var actual = sut.Map(f);
+
+            actual.Count.Should().Be(1);
+            actual.Single().Should().Be(4);
+        }
+
+        [Fact]
+        public void should_map_hashset_with_elements()
+        {
+            var sut = new HashSet<string>
+            {
+                null,
+                "",
+                "test",
+                "test2",
+                "test3"
+            };
+
+            int f(string s) => string.IsNullOrEmpty(s) ? 0 : s.Length;
+
+            var actual = sut.Map(f);
+
+            actual.Count.Should().Be(3);
+            actual.Single(x => x == 0).Should().Be(0);
+            actual.Single(x => x == 4).Should().Be(4);
+            actual.Single(x => x == 5).Should().Be(5);
         }
     }
 }
