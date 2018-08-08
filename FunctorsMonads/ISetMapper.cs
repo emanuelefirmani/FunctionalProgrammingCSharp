aad8d7deb03b79e02c3b607f992c75fa5176ce00
Emanuele Firmani <emanuele.firmani@gmail.com>
2018-08-08 15:06:35 +0200

    ISetMapper can map generic ISets

--- FunctorsMonads/ISetMapper.cs
+++ FunctorsMonads/ISetMapper.cs
@@ -8,7 +8,7 @@ namespace FunctorsMonads
 {
     public static class ISetMapper
     {
-        public static HashSet<int> Map(this ISet<string> input, Func<string, int> f)
+        public static HashSet<R> Map<T, R>(this ISet<T> input, Func<T, R> f)
         {
             return input.Select(f).ToHashSet();
         }
@@ -63,6 +63,25 @@ namespace FunctorsMonads
             actual.Single(x => x == 0).Should().Be(0);
             actual.Single(x => x == 4).Should().Be(4);
             actual.Single(x => x == 5).Should().Be(5);
+        }
+
+        [Fact]
+        public void should_map_hashset_with_integers_to_strings()
+        {
+            var sut = new HashSet<int>
+            {
+                0,1,2,3
+            };
+
+            string f(int n) => n.ToString();
+
+            var actual = sut.Map(f);
+
+            actual.Count.Should().Be(4);
+            actual.Single(x => x == "0").Should().Be("0");
+            actual.Single(x => x == "1").Should().Be("1");
+            actual.Single(x => x == "2").Should().Be("2");
+            actual.Single(x => x == "3").Should().Be("3");
         }
     }
 }
